f59b16813c860a1ca47f40b76ca2ef8db8b6b4b7
aliss <ac3721@student.ubc.ca>
2025-11-21 21:14:45 -0800

    Working code for one image

--- Generate SRT.py
+++ Generate SRT.py
@@ -12,36 +12,51 @@ start = []
 end = []
 text = []
 folder_dir = "Images"
+debug = False
+debug_words = False
+check = True
 
-def numbers(image, array):
+def numbers(image, array, count, i, debug = False):
     extracted_text = pytesseract.image_to_string(image)
-    # print(extracted_text)
     line = extracted_text.split('\n')
-    print(line)
+    if debug:
+        print(line)
 
     pattern = r'^\d{2}:\d{2}\.\d{3}$'
-    for time in line:
-        if len(time) > 0:
-            time = time.replace(',','')
-            if re.match(pattern, time):
-                array.append(time.replace('.',','))
+    time = line[0]
+    if len(time) > 0:
+        time = time.replace(',','')
+        if re.match(pattern, time):
+            array.append(time.replace('.',','))
+        else:
+            digits_only = re.sub(r'[^0-9]', '', time)
+            if len(digits_only) == 7:
+                formatted = digits_only[:2] + ':' + digits_only[2:4] + ',' + digits_only[4:]
+                array.append(formatted)
+            elif len(digits_only) == 5:
+                formatted = digits_only[:1] + ':' + digits_only[1:3] + ',' + digits_only[3:]
+                array.append(formatted)
             else:
-                digits_only = re.sub(r'[^0-9]', '', time)
-                if len(digits_only) == 7:
-                    formatted = digits_only[:2] + ':' + digits_only[2:4] + ',' + digits_only[4:]
-                    array.append(formatted)
-                elif len(digits_only) == 5:
-                    formatted = digits_only[:1] + ':' + digits_only[1:3] + ',' + digits_only[3:]
-                    array.append(formatted)
+                array.append('****** ' + time)
+    else:
+        count[i] += 1
+        if count[i] > 1:
+            array.append('******')
+            count[i] = 0
 
+def crop_image_to_boxes(image, box_height=50):
+    width, height = image.size
+    crops = []
+    for top in range(0, height, box_height):
+        bottom = min(top + box_height, height)
+        crop_box = (0, top, width, bottom)
+        cropped_img = image.crop(crop_box)
+        crops.append(cropped_img)
+    return crops
 
 for image_path in os.listdir(folder_dir):
-    # image = Image.open(image_path)
     base_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
     _, binary_image = cv2.threshold(base_image, 200, 255, cv2.THRESH_BINARY)
-    # kernel = np.ones((2,2), np.uint8)
-    # thicker_text = cv2.erode(binary_image, kernel, iterations=1)
 
     image = Image.fromarray(binary_image)
     width, height = image.size
@@ -52,7 +67,6 @@ for image_path in os.listdir(folder_dir):
         (image.width * scale_factor, image.height * scale_factor),
         resample=Image.LANCZOS
     )
-    # resized_image.show()
 
     left_box = (0, 0, box_width, height)
     middle_box = (box_width, 0, width - box_width - 1, height)
@@ -61,24 +75,40 @@ for image_path in os.listdir(folder_dir):
     left_section = image.crop(left_box)
     middle_section = image.crop(middle_box)
     right_section = image.crop(right_box)
+    count = [0, 0]
 
-    left_section.show()
-    print("start")
-    numbers(left_section, start)
-    print(start)
-    print("end")
-    numbers(right_section, end)
-    print(end)
+    if debug:
+        left_section.show()
+    left_boxes = crop_image_to_boxes(left_section, 50)
+    for line_img in left_boxes:
+        numbers(line_img, start, count, 0, debug)
+    if check:
+        print("start")
+        print(start)
+
+    if debug:
+        right_section.show()
+    right_boxes = crop_image_to_boxes(right_section, 50)
+    for line_img in right_boxes:
+        numbers(line_img, end, count,  1, debug)
+                
+    if check:
+        print("end")
+        print(end)
 
     config = '-l eng'
     all_text = pytesseract.image_to_string(middle_section, config=config)
-    print(all_text)
+    if debug_words:
+        print(all_text)
 
     line = all_text.split('\n')
     for words in line:
         if len(words) > 0:
             words = words.replace('|', 'I')
             text.append(words)
+    if check:
+        print(text)
+    print(len(start), len(end), len(text))
 
 with open('Output/output.srt', 'w') as output_file:
     for i in range(len(start)):
