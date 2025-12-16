32aab698edac65ce04549a0b2c1055bbf11ea34b
aliss <ac3721@student.ubc.ca>
2025-12-15 23:24:49 -0800

    Get all words

--- Input Video/Get Transcript.py
+++ Input Video/Get Transcript.py
@@ -21,7 +21,7 @@ def find_text(img, debug = False):
         plt.imshow(cv2.cvtColor(blue_text_img, cv2.COLOR_BGR2RGB))
         plt.axis("off")
         plt.show()
-    print(cv2.countNonZero(mask))
+        print(cv2.countNonZero(mask))
     return cv2.countNonZero(mask), blue_text_img
 
 def find_template(image, debug= False):
@@ -36,17 +36,20 @@ def find_template(image, debug= False):
         largest_contour = max(contours, key=cv2.contourArea)
             
         x, y, w, h = cv2.boundingRect(largest_contour)
-        x = max(0, x)
-        y = max(0, y)
-        w = min(image.shape[1] - x, w)
-        h = min(image.shape[0] - y, h )
+        padding = 15
+        x = max(0, x + padding)
+        y = max(0, y + padding)
+        w = min(image.shape[1] - x, w - 2 * padding)
+        h = min(image.shape[0] - y, h - 2 * padding)
         coord = [y, y+h, x, x+w]
         
         if debug:
-            cropped = image[y:y+h, x:x+w]
+            copy = image.copy()
+            cv2.drawContours(copy, [largest_contour], -1, (0, 255, 0), 3)
+            # cropped = image[y:y+h, x:x+w]
             
             # cv2.imwrite('cropped.jpg', cropped)
-            cv2.imshow('Cropped', cropped)
+            cv2.imshow('Contours', copy)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
     return coord
@@ -64,42 +67,64 @@ prev = None
 text = []
 change_timestamps = []
 coord = None
+status = False
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    present_pixel, filtered = find_text(frame)
-    if present_pixel > 5000:
-        if prev_state is None:
-            coord = find_template(frame)
+    if prev_state is None:
+        present_pixel, _ = find_text(frame)
+
+        if present_pixel > 6000:
+            # print(present_pixel)
+            coord = find_template(frame, debug=True)
             cropped = frame[coord[0]:coord[1], coord[2]:coord[3]]
 
+            present_pixel, filtered = find_text(cropped)
             prev_state = filtered
             prev = present_pixel
-            text.append(frame)
-        elif present_pixel - prev > 1000: #filtered != prev_state and 
-            # Color state changed at this frame
+            text.append(cropped)
+
             timestamp_sec = frame_index / fps
             change_timestamps.append(timestamp_sec)
+            print(prev, timestamp_sec)
+            status = True
+    else:
+        cropped = frame[coord[0]:coord[1], coord[2]:coord[3]]
+        present_pixel, filtered = find_text(cropped)
+
+        if present_pixel < 1000:
+            if status == True:
+                text.append(None)
+                timestamp_sec = frame_index / fps
+                change_timestamps.append(timestamp_sec)
+                print(prev, present_pixel, timestamp_sec, 'None')
+                status = False
+
+        elif abs(present_pixel - prev) > 500:
             prev_state = filtered
-            text.append(frame)
-            print(timestamp_sec)
+            prev = present_pixel
+            text.append(cropped)
+            
+            timestamp_sec = frame_index / fps
+            change_timestamps.append(timestamp_sec)
+            print(prev, present_pixel, timestamp_sec)
+            status = True
 
     frame_index += 1
 
 cap.release()
 
-print("Color-change timestamps (seconds):")
-for t in change_timestamps:
-    print(t)
+# print("Color-change timestamps (seconds):")
+# for t in change_timestamps:
+#     print(t)
 
 for frame in text:
-    plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    plt.axis("off")
-    plt.show()
-
-print("Color-change timestamps (seconds):")
-for t in change_timestamps:
-    print(t)
+    if frame is not None:
+        plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        plt.axis("off")
+        plt.show()
+    # else:
+    #     print("None")
